ddcba51e5853b8e1acdf0ef7acc605ee911af241
Miodini <84649065+Miodini@users.noreply.github.com>
2022-02-11 17:59:32 -0300

    ROTEIROOOOO
    Acho q o roteiro ta funcionando. Coloquei como usar no manual (tem q chamar a função organiza antes). Tmb mudei o nome da lista de eventos.

--- arquivo_insercao.pl
+++ arquivo_insercao.pl
@@ -1,2 +1,9 @@
 %Grupo 2
 %Gustavo Rosseto Leticio, Lucas Spagnol, Lucas Mondini, Pedro Gonçalves, Vinícius Brunheroto
+evento("Evento1", 3).
+evento("Evento2", 4).
+evento("Evento3", 5).
+evento("Evento4", 7).
+evento("Evento5", 8).
+evento("Evento6", 5).
+evento("Evento7", 2).
--- projeto2.pl
+++ projeto2.pl
@@ -1,31 +1,72 @@
 %Grupo 2
 %Gustavo Rosseto Leticio, Lucas Spagnol, Lucas Mondini, Pedro Gonçalves, Vinícius Brunheroto
 :-dynamic evento/2.
-:-dynamic lista/1.
-lista([]).
+:-dynamic eventos/1.
+:-dynamic roteiro/1.
+eventos([]).
+roteiro([]).
+
+if(Condição, Then, _) :- Condição, !, Then.
+if(_,_,Else) :- Else.
 
 %Adiciona evento no banco
 adiciona(Nome, Duracao) :-
     Duracao > 0, 
-    Duracao < 672, % 28 dias em horas (passível de alteração)
+    Duracao < 24,
     assertz(evento(Nome, Duracao)),
-	retract(lista(_)),
+	retract(eventos(_)),
     findall(evento(X,Y), evento(X, Y), L),
-    asserta(lista(L)).
+    asserta(eventos(L)).
 
 %Remove evento do banco
 remove(Nome) :-
     retract(evento(Nome, _)),
-    retract(lista(_)),
+    retract(eventos(_)),
     findall(evento(X,Y), evento(X, Y), L),
-    asserta(lista(L)).
+    asserta(eventos(L)).
 
 %Data e verificação
-data(Dia,Hora):- Dia>0, Dia<29, Hora>0, Hora <25.
+data(Dia, Hora):- Dia>0, Dia<29, Hora>0, Hora <25.
 
 %Roteiro
-roteiro(Nome,Duracao,Dia,Hora):-evento(Nome,Duracao),data(Dia,Hora).
+horario(Nome,Duracao,Dia,Hora):- evento(Nome,Duracao), data(Dia,Hora).
 
+organizaRecursao([], Roteiro, _, _, _, _, _):- % Critério de parada (salva no fato dinâmico)
+    retract(roteiro(_)),
+    asserta(roteiro(Roteiro)).
+
+organizaRecursao([EvHead | EvTail], Roteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, FimUltEvento):-
+    EvHead = evento(_, Duracao),
+    Duracao < ÚltimaHora - PrimeiraHora, %Falha caso a duração do evento seja maior que o intervalo de hora liberado para alocação
+    FimNovoEvento is FimUltEvento + Duracao,
+    	%se hora passou dos limites:
+    	%Se FimNovoEvento >= ÚltimaHora:
+    	%	FimUltEvento = PrimeiraHora
+        %	FimNovoEvento = PrimeiraHora + Duracao
+        %	PrimeiroDia = PrimeiroDia + 1
+    	
+    if(FimNovoEvento >= ÚltimaHora, 
+    (%THEN
+       	NovoPrimeiroDia is PrimeiroDia + 1,
+        NovoFimNovoEvento is PrimeiraHora + Duracao,
+        append(Roteiro, [horario(EvHead, data(NovoPrimeiroDia, PrimeiraHora))], NovoRoteiro),
+        organizaRecursao(EvTail, NovoRoteiro, NovoPrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, NovoFimNovoEvento)
+    ),
+    (%ELSE
+       	PrimeiroDia =< ÚltimoDia, % Falha se o dia atual for maior que o ultimo
+       	append(Roteiro, [horario(EvHead, data(PrimeiroDia, FimUltEvento))], NovoRoteiro),
+        organizaRecursao(EvTail, NovoRoteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, FimNovoEvento)
+     )).
+
+%Como chamar o organizador: organiza(eventos, [], PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora).
+%O roteiro fica salvo no fato dinâmico roteiro/1.
+organiza([EvHead | EvTail], Roteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora) :-
+    EvHead = evento(_, Duracao),
+    Duracao < ÚltimaHora - PrimeiraHora, %Falha caso a duração do evento seja maior que o intervalo de hora liberado para alocação
+    FimNovoEvento is PrimeiraHora + Duracao,
+    append(Roteiro, [horario(EvHead, data(PrimeiroDia, PrimeiraHora))], NovoRoteiro),
+    organizaRecursao(EvTail, NovoRoteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, FimNovoEvento). 
+    
 imprimeLista(Roteiro,Dia):-
     Roteiro=roteiro(evento(Nome,Duracao),data(DiaEvento,Hora)),
     Dia=:=DiaEvento,
