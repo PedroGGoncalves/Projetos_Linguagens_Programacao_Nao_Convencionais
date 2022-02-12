2758559a773d96d07bdada1adba24e3e20bdcec0
Miodini <84649065+Miodini@users.noreply.github.com>
2022-02-11 22:47:58 -0300

    Quase tudo pronto
    Falta as funções de arquivo, receber eventos via arquivo prolog

--- projeto2.pl
+++ projeto2.pl
@@ -1,40 +1,70 @@
 %Grupo 2
 %Gustavo Rosseto Leticio, Lucas Spagnol, Lucas Mondini, Pedro Gonçalves, Vinícius Brunheroto
+
+%%Setup do banco de dados dinâmico
 :-dynamic evento/2.
-:-dynamic eventos/1.
+:-dynamic listaEventos/1.
 :-dynamic roteiro/1.
-eventos([]).
+:-dynamic dia_inicio/1.
+:-dynamic dia_fim/1.
+:-dynamic hora_inicio/1.
+:-dynamic hora_fim/1.
+listaEventos([]).
 roteiro([]).
 
+%%Especificação do if then else
 if(Condição, Then, _) :- Condição, !, Then.
 if(_,_,Else) :- Else.
 
-%Adiciona evento no banco
-adiciona(Nome, Duracao) :-
-    Duracao > 0, 
-    Duracao < 24,
-    assertz(evento(Nome, Duracao)),
-	retract(eventos(_)),
-    findall(evento(X,Y), evento(X, Y), L),
-    asserta(eventos(L)).
+%%Verifica os limites válidos para dia e hora
+data(Dia, Hora):- Dia > 0, Dia < 29, Hora > 0, Hora < 25.
 
-%Remove evento do banco
+%%Gerenciamento de eventos
+%Adiciona eventos dinamicamente e reorganiza o roteiro
+adiciona(Nome, Duracao) :-
+    assertz(evento(Nome, Duracao)),
+	retract(listaEventos(_)),
+    findall(evento(X,Y), evento(X, Y), L),
+    asserta(listaEventos(L)),
+    %necessita chamar a função roteiroInf antes 
+    dia_inicio(PrimeiroDia), dia_fim(ÚltimoDia), hora_inicio(PrimeiraHora), hora_fim(ÚltimaHora),
+    organiza(L, [], PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora). 
+
+%Remove o evento de nome Nome do banco de dados
 remove(Nome) :-
     retract(evento(Nome, _)),
-    retract(eventos(_)),
+    retract(listaEventos(_)),
     findall(evento(X,Y), evento(X, Y), L),
-    asserta(eventos(L)).
+    asserta(listaEventos(L)).
 
-%Data e verificação
-data(Dia, Hora):- Dia>0, Dia<29, Hora>0, Hora <25.
+%%Roteiro
+roteiroInf :-
+    print("Digite primeiro dia:"),
+    nl,
+    read(Primeirodia),
+    print("Digite ultimo dia:"),
+    nl,
+    read(Ultimodia),
+    print("Digite primeira hora de cada dia:"), 
+    nl,
+    read(Primeirahora),
+    print("Digite a ultima hora de cada dia: "),
+    nl,
+    read(Ultimahora),
+    data(Primeirodia, Primeirahora),
+    data(Ultimodia, Ultimahora),
+    asserta(dia_inicio(Primeirodia)),
+    asserta(dia_fim(Ultimodia)),
+    asserta(hora_inicio(Primeirahora)),
+    asserta(hora_fim(Ultimahora)).
 
-%Roteiro
-horario(Nome,Duracao,Dia,Hora):- evento(Nome,Duracao), data(Dia,Hora).
-
-organizaRecursao([], Roteiro, _, _, _, _, _):- % Critério de parada (salva no fato dinâmico)
+%%Organizador
+%Critério de parada (salva no banco de dados dinâmico)
+organizaRecursao([], Roteiro, _, _, _, _, _):- 
     retract(roteiro(_)),
     asserta(roteiro(Roteiro)).
 
+%Parte recursiva do procedimento de organização do roteiro
 organizaRecursao([EvHead | EvTail], Roteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, FimUltEvento):-
     EvHead = evento(_, Duracao),
     Duracao < ÚltimaHora - PrimeiraHora, %Falha caso a duração do evento seja maior que o intervalo de hora liberado para alocação
@@ -58,7 +88,8 @@ organizaRecursao([EvHead | EvTail], Roteiro, PrimeiroDia, ÚltimoDia, PrimeiraHo
         organizaRecursao(EvTail, NovoRoteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, FimNovoEvento)
      )).
 
-%Como chamar o organizador: organiza(eventos, [], PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora).
+%Procedimento inicial de organização do roteiro
+%Como chamar o organizador: listaEventos(E), organiza(E, [], PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora).
 %O roteiro fica salvo no fato dinâmico roteiro/1.
 organiza([EvHead | EvTail], Roteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora) :-
     EvHead = evento(_, Duracao),
@@ -66,56 +97,59 @@ organiza([EvHead | EvTail], Roteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, Últ
     FimNovoEvento is PrimeiraHora + Duracao,
     append(Roteiro, [horario(EvHead, data(PrimeiroDia, PrimeiraHora))], NovoRoteiro),
     organizaRecursao(EvTail, NovoRoteiro, PrimeiroDia, ÚltimoDia, PrimeiraHora, ÚltimaHora, FimNovoEvento). 
-    
-imprimeLista(Roteiro,Dia):-
-    Roteiro=roteiro(evento(Nome,Duracao),data(DiaEvento,Hora)),
-    Dia=:=DiaEvento,
-    format('~a~d~d~d~n', [Nome,Duracao,DiaEvento,Hora]).
 
-%Calendario
-diaSemana(1,terça).
-diaSemana(2,quarta).
-diaSemana(3,quinta).
-diaSemana(4,sexta).
-diaSemana(5,sabado).
-diaSemana(6,domingo).
-diaSemana(7,segunda).
-diaSemana(N,Nome):-
+%%Calendario
+%Fatos e regras para associação dos dias da semana
+diaSemana(1, terça).
+diaSemana(2, quarta).
+diaSemana(3, quinta).
+diaSemana(4, sexta).
+diaSemana(5, sabado).
+diaSemana(6, domingo).
+diaSemana(7, segunda).
+diaSemana(N, Nome):-
     Nmenos is N - 7,
     diaSemana(Nmenos, Nome).
-dia(N,Roteiro):-
-   (
-    N=<28,
-    diaSemana(N,Nome),
-    format('Dia:~d ~a ~n',[N,Nome]),
-    imprimeEventos(N,Roteiro),
-    Proximo is N+1,
-    dia(Proximo,Roteiro)
-    )
-    ;
+%Imprime o calendário recursivamente
+dia(N, Roteiro):-
+    (     
+    	N  =< 28,
+      	diaSemana(N, Nome),
+        format('Dia:~d ~a ~n', [N, Nome]),
+        imprimelistaEventos(N, Roteiro),
+        Proximo is N+1,
+        dia(Proximo, Roteiro)
+    );
     true.
     
-%---------------------PRECISA TESTAR--------------------------------------%
-%requisito2
-imprimeData(Horario,Dia) :-
-    Horario = horario(evento(Nome,Duração), data (DiaEvento, Hora)),
+%Imprime informações do evento se estiver no dia certo
+imprimeData(Horario, Dia) :-
+    Horario = horario(evento(Nome, Duração), data(DiaEvento, Hora)),
     Dia =:= DiaEvento,  %se isso for verdade, printa
-    format("~a ~d ~d ~d~n",[Nome,Duração,DiaEvento,Hora]).
+    format("~a: Dia ~d às ~d horas. Duração: ~d horas.~n",[Nome, DiaEvento, Hora, Duração]).
 
-%caso base
-imprimeEventos(_,[]).
+%Caso base
+imprimelistaEventos(_,[]).
 
-%Recursão( passa o dia que deve imprimir e o roteiro)
-imprimeEventos(N, Roteiro) :-
-   Roteiro = [Cabeça|Cauda], % divide numa lista com cabeça e cauda
-   (
-    imprimeData(Cabeça, N) % subrotina
-    ;
-    imprimeEventos(N, Cauda) % recursão, chama ele mesmo passando a cauda
-   ).
-%-------------------------------------------------------------------------%
-    
-    
-calendario:-dia(1,Roteiro).
-%Requisito 2 para arquivo
-calendarioNoArquivo:-tell('requisito2.txt'), calendario, told.
+%Recursão para procurar se há eventos para imprimir no dia N (passa o dia e o roteiro)
+imprimelistaEventos(N, [Cabeça|Cauda]) :-
+    imprimeData(Cabeça, N), fail; % subrotina (não sei pq mas ele só roda o que está dps do ; se der false)  
+    imprimelistaEventos(N, Cauda). % recursão, chama ele mesmo passando a cauda
+
+%Requisito 1
+imprimeLista:-
+    roteiro(R),
+    write(R).    
+%Requisito 2   
+calendarioRoteiro:- 
+    roteiro(R),
+    dia(1, R), !.
+
+%Requisito 3
+calendarioSimples:-
+    dia(1, []), !.
+
+%Requisito 2 para arquivo (precisa testar no Linux)
+calendarioRoteiroNoArquivo:- tell('requisito2.txt'), calendarioRoteiro, told.
+%Requisito 3 para arquivo (precisa testar no Linux)
+calendarioSimplesNoArquivo:- tell('requisito2.txt'), calendarioSimples, told.
